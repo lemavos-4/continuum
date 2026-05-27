2dcf73d0817a4ca057adaee06d509f785abb89bf
lemavos-4 <rickkrkz2025@gmail.com>
2026-05-27 23:15:21 +0000

    v10.16

--- commit.sh
+++ commit.sh
@@ -8,7 +8,7 @@ VERMELHO='\033[0;31m'
 NC='\033[0m'
 
 # --- VERSÃO ATUAL ---
-# VERSION: 10.14
+# VERSION: 10.15
 
 # Pega a versão atual do próprio arquivo
 ULTIMA_V=$(grep "^# VERSION:" "$0" | cut -d ' ' -f 3)
